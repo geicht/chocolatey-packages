bdbddf75715e43b7ed3f934002458b87f0ed5ac0
Chocolatey <chocolatey@realdimensions.net>
2022-02-09 18:20:55 +0000

    AU: 1 updated - docker-cli [skip ci] https://gist.github.com/976b73046e5c0aad398ce72a508532a6/536954e1b28a16e04d3212200dbd651df077299b

--- docker-cli/tools/chocolateyinstall.ps1
+++ docker-cli/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference  = 'Stop';
 $toolsDir               = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64                  = ''
-$checksum64             = ''
+$url64                  = 'https://download.docker.com/win/static/stable/x86_64/docker-20.10.12.zip'
+$checksum64             = 'bd3775ada72492aa1f3c2edb3e81663bd128b9d4f6752ef75953a6af7c219c81'
 $checksumType64         = 'sha256'
 
 $packageArgs = @{
