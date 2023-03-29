4da89428afcdf68fb14bf772a1fa09a81f39b063
Chocolatey <chocolatey@realdimensions.net>
2023-03-29 01:50:17 +0000

    AU: 2 updated - mambaforge miniforge3 [skip ci] https://gist.github.com/geicht/976b73046e5c0aad398ce72a508532a6/1d0980c3d9e91ec8a6f002b84d74e4294becab08

--- mambaforge/tools/chocolateyInstall.ps1
+++ mambaforge/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 . $(Join-Path $packageToolsDir "helpers.ps1")
 $packageParameters  = Get-PackageParameters
 $silentArgs         = Get-SilentArgs($packageParameters)
-$installerName      = 'Mambaforge-22.11.1-4-Windows-x86_64.exe'
+$installerName      = 'Mambaforge-23.1.0-0-Windows-x86_64.exe'
 $installerPath      = Join-Path $packageToolsDir $installerName
 
 $packageArgs = @{
