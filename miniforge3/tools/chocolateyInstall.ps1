6794ed4061cc816187af36a6fa65d2a8f5d34b77
Chocolatey <chocolatey@realdimensions.net>
2025-05-11 01:41:46 +0000

    AU: 1 updated - miniforge3 [skip ci] https://gist.github.com/geicht/976b73046e5c0aad398ce72a508532a6/c3b8dd675a613b2bc8d839e2e08caa6fa22b5a61

--- miniforge3/tools/chocolateyInstall.ps1
+++ miniforge3/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 . $(Join-Path $packageToolsDir "helpers.ps1")
 $packageParameters  = Get-PackageParameters
 $silentArgs         = Get-SilentArgs($packageParameters)
-$installerName      = 'Miniforge3-25.3.0-1-Windows-x86_64.exe'
+$installerName      = 'Miniforge3-25.3.0-3-Windows-x86_64.exe'
 $installerPath      = Join-Path $packageToolsDir $installerName
 
 $packageArgs = @{
